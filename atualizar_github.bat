d54c645e0b314a756734487e12cfc67fd17bcf7e
Hyuryx <hyuryx@users.noreply.github.com>
2026-06-28 21:52:31 -0300

    Adiciona fotos de codigo de intensidade e comandos da QSV

---
+++ atualizar_github.bat
@@ -0,0 +1,10 @@
+@echo off
+echo Sincronizando com o GitHub...
+"C:\Program Files\Git\cmd\git.exe" add .
+"C:\Program Files\Git\cmd\git.exe" commit -m "Atualizacao pelo script"
+"C:\Program Files\Git\cmd\git.exe" push origin main
+echo.
+echo =========================================
+echo Atualizacao enviada com sucesso!
+echo =========================================
+pause
