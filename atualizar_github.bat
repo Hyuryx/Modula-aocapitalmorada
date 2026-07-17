1d2a316fd091564316849d2e1566c47d006c4ebf
Hyuryx <hyuryx@users.noreply.github.com>
2026-07-17 17:00:26 -0300

    Atualizacao pelo script

--- atualizar_github.bat
+++ atualizar_github.bat
@@ -1,5 +1,6 @@
 @echo off
 echo Sincronizando com o GitHub...
+"C:\Program Files\Git\cmd\git.exe" pull origin main
 "C:\Program Files\Git\cmd\git.exe" add .
 "C:\Program Files\Git\cmd\git.exe" commit -m "Atualizacao pelo script"
 "C:\Program Files\Git\cmd\git.exe" push origin main
