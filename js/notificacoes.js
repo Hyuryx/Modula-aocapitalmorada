d110f4e99a5a8c04426432b65a324e17853d47ae
Hyuryx <hyuryx@users.noreply.github.com>
2026-08-22 03:20:10 -0300

    Atualizacao automatica via script

--- js/notificacoes.js
+++ js/notificacoes.js
@@ -554,13 +554,7 @@ window.addEventListener('tabChanged', async (e) => {
       }
     }
 
-    // Hardcoded fallback para Evento de Login
-    if (!avisoDestaque && targetId === 'login-event') {
-      avisoDestaque = {
-        imagem: 'assets/eventos/luta.png',
-        descricao: `🏆 **ESTÁ OFICIALMENTE ABERTO O GRANDE TORNEIO DE LUTAS DA SAÚDE CAPITALENSE** 🏆\n\nA hora de provar quem tem a mão mais pesada da cidade chegou! Prepare-se para uma semana de combates intensos, muita adrenalina e prêmios milionários. Junte sua coragem e venha fazer história da Capital!\n\n📅 **Cronograma do Evento**\nPeríodo de Inscrições: 10/08 até 16/08 *(Não perca o prazo!)*\nInício das Lutas (Fase Classificatória): 17/08\nGrande Semifinal e Final: 21/08 (Sexta-feira), às 22h\n\n📍 **Localização**\nComunidade da Nova Holanda *(A arena será montada no coração da comunidade!)*\n\n💰 **Premiação**\nConfira o que os vencedores vão levar para casa:\n\n🥇 **1º Lugar** R$ 1.200.000 \n🥈 **2º Lugar** R$ 600.000 \n🥉 **3º Lugar** R$ 200.000`
-      };
-    }
+
 
     const modalDestaque = document.getElementById('modal-destaque');
     if (modalDestaque && avisoDestaque) {
